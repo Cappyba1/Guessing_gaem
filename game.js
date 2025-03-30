0b87d4b216a08e583bee85d44d4dc144b4ac945e
Cappyba1 <otricaldemon@gmail.com>
2025-03-30 19:58:26 +0545

    Initial commit

---
+++ game.js
@@ -0,0 +1,26 @@
+// Pick a random number between 1 and 100
+ let secretNumber = Math.floor(Math.random() * 100) + 1;
+function resetGame(){
+    secretNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+// Function to check the guess
+function checkGuess() {
+    // Get the user's guess from the input box
+    let userGuess = document.getElementById("guessInput").value;
+    userGuess = Number(userGuess); // Convert it to a number (input gives text)
+
+    // Get the message paragraph to update it
+    let message = document.getElementById("message");
+
+    // Check the guess and update the message
+    if (userGuess < 1 || userGuess > 100) {
+        message.innerHTML = "Pick a number between 1 and 100, dumbass!";
+    } else if (userGuess < secretNumber) {
+        message.innerHTML = "Too low! Try again.";
+    } else if (userGuess > secretNumber) {
+        message.innerHTML = "Too high! Try again.";
+    } else if (userGuess == secretNumber) {
+        message.innerHTML = "You got it! Nice one!";
+    }
+}
